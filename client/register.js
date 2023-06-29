5f8491c6ac171689e4771d52e76dea3665e99568
anthonychan1211 <anthonychan1211@gmail.com>
2023-06-29 16:51:28 +0100

    form data fetch done

--- client/register.js
+++ client/register.js
@@ -1,8 +1,23 @@
 const submitButton = document.querySelector(".submit-button");
 
-submitButton.addEventListener("click", (e) => {
+submitButton.addEventListener("click", async (e) => {
   e.preventDefault();
   const form = document.querySelector(".form");
-  console.log(form.username.value);
-  console.log(form.password.value);
+  const username = form.username.value;
+  const password = form.password.value;
+  try {
+    const res = await fetch("http://localhost:3000/register", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    const data = await res.json();
+    if (res.status === 200) {
+      alert("Register succesfully");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
