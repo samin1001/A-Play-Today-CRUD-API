8342c7bfbb7def2a841f3861a99ab89dfed239af
Samin Chowdhury <samin@Samins-Mac-mini.home>
2023-06-29 11:42:34 +0100

    added createPlay endpoint

--- server/models/Play.js
+++ server/models/Play.js
@@ -13,6 +13,14 @@ class Play{
         const {rows} = await db.query(query,[id])
         return rows[0]
     }
+
+    static async createPlay(play){
+        const {play_name, price, date_time_played, duration, cover_photo} = play
+        const query = 'INSERT INTO theatre_plays (play_name, price, date_time_played, duration, cover_photo) VALUES ($1, $2, $3, $4, $5) RETURNING *'
+        const values = [play_name, price, date_time_played, duration, cover_photo]
+        const {rows} = await db.query(query, values)
+        return rows[0]
+    }
 }
 
 module.exports = Play
