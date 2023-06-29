cd26781e9cde4d471bbc81b8528891b0268a0e4d
anthonychan1211 <anthonychan1211@gmail.com>
2023-06-29 16:41:01 +0100

    adding fetch on register page

--- client/register.js
+++ client/register.js
@@ -1,1 +1,8 @@
-console.log("register");
+const submitButton = document.querySelector(".submit-button");
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const form = document.querySelector(".form");
+  console.log(form.username.value);
+  console.log(form.password.value);
+});
